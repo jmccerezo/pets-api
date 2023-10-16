3c8731b842711ecf053871c05a1c72951a1751e5
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-10-16 16:32:03 +0800

    Refactor import

--- src/pet/controllers/pet.controller.ts
+++ src/pet/controllers/pet.controller.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import { Request, Response } from "express";
 import {
   create,
   findAll,
@@ -7,31 +7,31 @@ import {
   findByIdAndDelete,
 } from "../services/pet.service";
 
-const addPet = async (req: express.Request, res: express.Response) => {
+const addPet = async (req: Request, res: Response) => {
   await create(req.body)
     .then((data) => res.status(200).json(data))
     .catch((err: Error) => res.status(500).send(err));
 };
 
-const getAllPets = async (req: express.Request, res: express.Response) => {
+const getAllPets = async (req: Request, res: Response) => {
   await findAll()
     .then((data) => res.status(200).json(data))
     .catch((err: Error) => res.status(500).send(err));
 };
 
-const getOnePet = async (req: express.Request, res: express.Response) => {
+const getOnePet = async (req: Request, res: Response) => {
   await findById(req.params.id)
     .then((data) => res.status(200).json(data))
     .catch((err: Error) => res.status(500).send(err));
 };
 
-const editPet = async (req: express.Request, res: express.Response) => {
+const editPet = async (req: Request, res: Response) => {
   await findByIdAndUpdate(req.params.id, req.body)
     .then((data) => res.status(200).json(data))
     .catch((err: Error) => res.status(500).send(err));
 };
 
-const deletePet = async (req: express.Request, res: express.Response) => {
+const deletePet = async (req: Request, res: Response) => {
   await findByIdAndDelete(req.params.id)
     .then((data) => res.status(200).json(data))
     .catch((err: Error) => res.status(500).send(err));
