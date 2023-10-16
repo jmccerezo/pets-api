462da49eb56290454742a7e382d1ed80554faaa8
John Menard Cerezo <johnmenardccerezo@gmail.com>
2023-10-16 16:13:56 +0800

    Refactor services export

--- src/pet/services/pet.service.ts
+++ src/pet/services/pet.service.ts
@@ -1,22 +1,22 @@
 import PetModel from "../models/pet.model";
 
-export const create = async (data: any) => {
+const create = async (data: any) => {
   const newPet = await PetModel.create(data);
   return newPet;
 };
 
-export const findAll = async () => {
+const findAll = async () => {
   const pets = await PetModel.find();
   return pets;
 };
 
-export const findById = async (id: string) => {
+const findById = async (id: string) => {
   const pet = await PetModel.findById(id);
   if (!pet) return "Pet not found.";
   return pet;
 };
 
-export const findByIdAndUpdate = async (id: string, data: any) => {
+const findByIdAndUpdate = async (id: string, data: any) => {
   const updatedPet = await PetModel.findByIdAndUpdate(id, data, {
     new: true,
   });
@@ -24,8 +24,10 @@ export const findByIdAndUpdate = async (id: string, data: any) => {
   return updatedPet;
 };
 
-export const findByIdAndDelete = async (id: string) => {
+const findByIdAndDelete = async (id: string) => {
   const deletedPet = await PetModel.findByIdAndDelete(id);
   if (!deletedPet) return "Pet not found.";
   return deletedPet;
 };
+
+export { create, findAll, findById, findByIdAndUpdate, findByIdAndDelete };
