dc224d6aa53e8cf517d318d227f59e333251d4a8
JM Cerezo <johnmenardccerezo@gmail.com>
2023-10-25 21:39:48 +0800

    Refactor services

--- src/pet/services/pet.services.ts
+++ src/pet/services/pet.services.ts
@@ -1,33 +1,25 @@
 import PetModel from "../models/pet.model";
 
 const create = async (data: any) => {
-  const newPet = await PetModel.create(data);
-  return newPet;
+  return await PetModel.create(data);
 };
 
 const findAll = async () => {
-  const pets = await PetModel.find();
-  return pets;
+  return await PetModel.find();
 };
 
 const findById = async (id: string) => {
-  const pet = await PetModel.findById(id);
-  if (!pet) return "Pet not found.";
-  return pet;
+  return await PetModel.findById(id);
 };
 
 const findByIdAndUpdate = async (id: string, data: any) => {
-  const updatedPet = await PetModel.findByIdAndUpdate(id, data, {
+  return await PetModel.findByIdAndUpdate(id, data, {
     new: true,
   });
-  if (!updatedPet) return "Pet not found.";
-  return updatedPet;
 };
 
 const findByIdAndDelete = async (id: string) => {
-  const deletedPet = await PetModel.findByIdAndDelete(id);
-  if (!deletedPet) return "Pet not found.";
-  return deletedPet;
+  return await PetModel.findByIdAndDelete(id);
 };
 
 export { create, findAll, findById, findByIdAndUpdate, findByIdAndDelete };
